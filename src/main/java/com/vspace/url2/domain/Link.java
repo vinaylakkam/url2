9676e8259224f4f7d5ff2a6e80363b33e9752f49
vinaylakkam <vinay.lakkam@gmail.com>
2014-04-09 16:20:04 +0530

    dao added

--- src/main/java/com/vspace/url2/domain/Link.java
+++ src/main/java/com/vspace/url2/domain/Link.java
@@ -1,10 +1,35 @@
 package com.vspace.url2.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Link {
+
+	@Id
+	private String id;
 
 	String url1;
 
 	String url2;
+
+	public Link() {
+		super();
+	}
+
+	public Link(String url1, String url2) {
+		super();
+		this.url1 = url1;
+		this.url2 = url2;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getUrl1() {
 		return url1;
@@ -24,6 +49,7 @@ public class Link {
 
 	@Override
 	public String toString() {
-		return "Link [url1=" + url1 + ", url2=" + url2 + "]";
+		return "Link [id=" + id + ", url1=" + url1 + ", url2=" + url2 + "]";
 	}
+
 }
--- src/main/java/com/vspace/url2/service/ShortenerService.java
+++ src/main/java/com/vspace/url2/service/ShortenerService.java
@@ -1,13 +1,35 @@
 package com.vspace.url2.service;
 
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.vspace.url2.dao.LinkDao;
+import com.vspace.url2.domain.Link;
 import com.vspace.url2.util.NumberConverter;
 
 @Component
 public class ShortenerService {
 
-	public String shortIt(int number) {
+	@Autowired
+	LinkDao linkDao;
+
+	public String shortIt(Integer number) {
 		return NumberConverter.toBase62(number);
+	}
+
+	public String saveLink(String url1) {
+
+		String url2 = shortIt(new Random().nextInt());
+
+		Link link = new Link(url1, url2);
+		
+		Boolean success = linkDao.save(link);
+		if (success) {
+			return url2;
+		}
+
+		return null;
 	}
 }
